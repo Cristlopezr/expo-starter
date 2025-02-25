a1aeb32018f28eb8f5b99088b01a219fd181747b
Cristian <cristianlopezr.931@gmail.com>
2025-02-25 13:50:22 -0300

    First commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/* import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+const argv = yargs(hideBin(process.argv)).argv
+ */
+
+(async () => {
+    await main();
+})();
+
+async function main() {
+    console.log('App running');
+}
